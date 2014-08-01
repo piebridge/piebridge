7cd95fa9d899a45de6d99521240896ddf60762b7
05609@163.com <05609@163.com>
2014-08-01 17:52:56 +0800

    [fun]模板调整

--- piebridge/protected/components/Controller.php
+++ piebridge/protected/components/Controller.php
@@ -6,13 +6,14 @@
 error_reporting(E_ALL);
 class Controller extends CController
 {
-	private $_viewpath = '';
+
+	private $_viewFile = '';
 
 	/**
 	 * @var string the default layout for the controller view. Defaults to '//layouts/column1',
 	 * meaning using a single column layout. See 'protected/views/layouts/column1.php'.
 	 */
-	public $layout='//layouts/column1';
+	public $layout='/layouts/column1';
 	/**
 	 * @var array context menu items. This property will be assigned to {@link CMenu::items}.
 	 */
@@ -25,12 +26,11 @@ class Controller extends CController
 	public $breadcrumbs=array();
 
 
-	public function use_view($viewpath='')
+	public function use_view($viewName='')
 	{
-		if(empty($view_path)){
-			$viewpath = $this->getViewPath().DS.$this->action->id.'.tpl';
-		}
-		$this->_viewpath = $viewpath;
+		if(empty($viewName)) $viewName = $this->action->id;
+		$viewpath = $this->getViewFile($viewName);
+		$this->_viewFile = $viewpath;
 	}
 
 	/**
@@ -43,12 +43,74 @@ class Controller extends CController
 	}
 
 	/**
+	 * Smarty fetch()方法
+	 *
+	 */
+	public function fetch($view)
+	{
+		return Yii::app()->smarty->fetch($view);
+	}
+
+
+	/**
 	 * Smarty display()方法
 	 *
 	 */
 	public function display()
 	{
-		Yii::app()->smarty->display($this->_viewpath);
+		if($this->layout){
+			$view_html = Yii::app()->smarty->fetch($this->_viewFile);
+
+			Yii::app()->smarty->assign('content', $view_html);
+			$layoutFile=$this->getLayoutFile($this->layout);
+			$this->renderLayout($layoutFile);
+		}
+
+		Yii::app()->smarty->display(!empty($layoutFile) ? $layoutFile : $this->_viewFile);
 	}
+
+	public function renderLayout($layoutFile)
+	{
+		$layout = str_replace('html', 'php', $layoutFile);
+
+		include_once $layout;
+		$layoutname = basename($layout, '.php');
+		$layout = new $layoutname();
+		$layout->renderLayout();
+	}
+
+	public function resolveViewFile($viewName,$viewPath,$basePath,$moduleViewPath=null)
+	{
+		if(empty($viewName))
+			return false;
+
+		if($moduleViewPath===null)
+			$moduleViewPath=$basePath;
+
+		if(($renderer=Yii::app()->getViewRenderer())!==null)
+			$extension=$renderer->fileExtension;
+		else
+			$extension='.html';
+		if($viewName[0]==='/')
+		{
+			if(strncmp($viewName,'//',2)===0)
+				$viewFile=$basePath.$viewName;
+			else
+				$viewFile=$moduleViewPath.$viewName;
+		}
+		elseif(strpos($viewName,'.'))
+		$viewFile=Yii::getPathOfAlias($viewName);
+		else
+			$viewFile=$viewPath.DIRECTORY_SEPARATOR.$viewName;
+
+		if(is_file($viewFile.$extension))
+			return Yii::app()->findLocalizedFile($viewFile.$extension);
+		elseif($extension!=='.html' && is_file($viewFile.'.html'))
+		return Yii::app()->findLocalizedFile($viewFile.'.html');
+		else
+			return false;
+	}
+
+
 
 }
--- piebridge/protected/controllers/SiteController.php
+++ piebridge/protected/controllers/SiteController.php
@@ -29,7 +29,12 @@ class SiteController extends Controller
 	{
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
-		$this->render('index');
+
+		$this->use_view();
+		$this->assign('key', '345354');
+		$this->assign('key_left', 'left');
+
+		$this->display();
 	}
 
 	/**
